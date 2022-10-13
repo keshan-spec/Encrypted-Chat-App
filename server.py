b4c6b8adc2e31a433d377c3155a63ef5599eff07
keshan-spec <keshanth.programmer@gmail.com>
2022-10-13 14:14:15 +0100

    Solved error: Unicode 18

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import socket, threading, argparse
 import os, datetime
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Random import get_random_bytes
+from encryption.RSA import RSAEncryptionServer
 
 
 class Server:
@@ -12,19 +10,16 @@ class Server:
 
     def start_server(self):
         # Generate the public and private keys to share
-        # And a random secret key for AES
-        self.generate_keys()
-        secret_key = get_random_bytes(16)
+        rsa = RSAEncryptionServer()
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.clients = []
 
         self.s.bind((self.host, self.port))
         self.s.listen(100)
 
         print(f"[+] Server started on {self.host}:{self.port}")
 
+        self.clients = []
         self.username_lookup = {}
 
         """
@@ -42,11 +37,7 @@ class Server:
             self.clients.append(c)
 
             # Exchange the public key with the client
-            client_pub_key = self.send_pub_key(c)
-            # Encrypt the secret with the client public key
-            encrypted_secret = self.encrypt_secret(client_pub_key, secret_key)
-            # send the encrypted secret to the client
-            self.send_secret(c, encrypted_secret)
+            rsa.exchange_keys(c)
 
             # Broadcast the new user to all the clients
             self.broadcast(f"{username} has joined the server.", c)
@@ -67,56 +58,6 @@ class Server:
             if connection == c:
                 continue
             connection.send(msg.encode())
-
-    # Generate the public and private key pair
-    @staticmethod
-    def generate_keys():
-        try:
-            private_key = RSA.generate(2048)
-            public_key = private_key.publickey()
-            private_key_pem = private_key.exportKey().decode()
-            public_key_pem = public_key.exportKey().decode()
-
-            with open("server_private_key.pem", "w") as priv:
-                priv.write(private_key_pem)
-            with open("server_public_key.pem", "w") as pub:
-                pub.write(public_key_pem)
-
-            return public_key
-        except Exception as e:
-            print(e)
-
-    # Encrypt the secret with the client public key
-    @staticmethod
-    def encrypt_secret(client_pub_key, secret_key):
-        try:
-            cpKey = RSA.importKey(client_pub_key)
-            cipher = PKCS1_OAEP.new(cpKey)
-            encrypted_secret = cipher.encrypt(secret_key)
-            return encrypted_secret
-        except Exception as e:
-            print(e)
-
-    @staticmethod
-    def send_secret(c, secret_key):
-        try:
-            c.send(secret_key)
-            print(" Secret key had been sent to the client ")
-
-        except Exception as e:
-            print(e)
-
-    # Exchanging the public key with the client
-    @staticmethod
-    def send_pub_key(c):
-        try:
-            public_key = RSA.importKey(open("server_public_key.pem", "r").read())
-            c.send(public_key.exportKey())
-            client_pub_key = c.recv(1024)
-            print(" Client public key had been received")
-            return client_pub_key
-        except Exception as e:
-            print(f"Error exchanging public key with client: {e}")
 
     def handle_client(self, c, _):
         while True:
@@ -148,11 +89,12 @@ class Server:
 def terminate(Server):
     while True:
         command = input("")
-        if command == "TERMINATE":
+        if command.lower() == "terminate":
             for conn in Server.clients:
                 conn.shutdown(socket.SHUT_RDWR)
             print(" All connections had been terminated")
-        break
+            break
+
     print(" Server is shut down")
     os._exit(0)
 
@@ -161,7 +103,18 @@ if __name__ == "__main__":
     # Port needed as argument whihc will be used to open the socket
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument(
-        "-p", "--port", type=int, required=True, help="port to run the server"
+        "-p",
+        "--port",
+        type=int,
+        required=True,
+        help="port to run the server",
+    )
+    arg_parse.add_argument(
+        "-e",
+        "--encrypt",
+        type=str,
+        required=True,
+        help="encryption type to use (AES or RSA)",
     )
     args = arg_parse.parse_args()
 
